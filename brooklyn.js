6c96bcebad0c89fd6a6701ab275dafe6cb50bd2e
tahminaahmedrima <tahminaahmedrima@users.noreply.github.com>
2020-10-25 11:43:16 +0600

    New file added

---
+++ brooklyn.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  window.onscroll = function () {
+    myFunction();
+  };
+
+  function myFunction() {
+    if (
+      document.body.scrollTop > 300 ||
+      document.documentElement.scrollTop > 300
+    ) {
+      document.getElementById("main-nav").className = "sticky";
+    } else {
+      document.getElementById("main-nav").className = "";
+    }
+  }
+});
